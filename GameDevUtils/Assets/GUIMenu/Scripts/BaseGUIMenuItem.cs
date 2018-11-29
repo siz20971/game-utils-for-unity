43538d68c713d2cb1db3d181c83547b862ec7ae8
NamKyoungsoo <siz20971@neowiz.com>
2018-11-29 19:16:24 +0900

    테스트용 유니티 onGUI 메뉴 기능 추가.

---
+++ GameDevUtils/Assets/GUIMenu/Scripts/BaseGUIMenuItem.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GUIMenu
+{
+    public abstract class BaseGUIMenuItem
+    {
+        public void DrawItem()
+        {
+            DrawItemImplement();
+        }
+
+        protected abstract void DrawItemImplement();
+
+        public void ProcessMenu()
+        {
+
+        }
+    }
+
+    public class ButtonMenuItem : BaseGUIMenuItem
+    {
+        private string buttonName;
+
+        public ButtonMenuItem(string buttonName)
+        {
+            this.buttonName = buttonName;
+        }
+
+        protected override void DrawItemImplement()
+        {
+            GUILayout.Button(buttonName);
+        }
+    }
+
+    //public class ToggleMenuItem : BaseGUIMenuItem
+    //{
+
+    //}
+}
---
+++ GameDevUtils/Assets/GUIMenu/Scripts/GUIMenu.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GUIMenu
+{
+    public enum DIRECTION
+    {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+
+    public class GUIMenu : MonoBehaviour
+    {
+        public bool IsOpened { get; private set; }
+
+        public int col = 3;
+        public int row = 3;
+
+        public void MoveCursor(DIRECTION direction)
+        {
+
+        }
+
+        public void SelectCurrentMenu()
+        {
+
+        }
+
+        private void OnGUI()
+        {
+            var tmp = new List<BaseGUIMenuItem>();
+
+            for (int i = 0; i < (col * row); i++)
+                tmp.Add(new ButtonMenuItem((i + 1) + " Btn"));
+
+            for (int i = 0; i < col; i++)
+            {
+                GUILayout.BeginHorizontal();
+                for (int j = 0; j < row; j++)
+                {
+                    int index = j * col + i;
+
+                    if (tmp.Count > index)
+                        tmp[index].DrawItem();
+                }
+                GUILayout.EndHorizontal();
+            }
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+                MoveCursor(DIRECTION.UP);
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+                MoveCursor(DIRECTION.DOWN);
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+                MoveCursor(DIRECTION.LEFT);
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+                MoveCursor(DIRECTION.RIGHT);
+
+            if (Input.GetKeyDown(KeyCode.Return))
+                SelectCurrentMenu();
+        }
+    }
+}
+
